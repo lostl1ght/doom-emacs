a91341ccfca958521e05cd33bfc0a1f1e405334a
Constantine Bulany <robpirojk@gmail.com>
2022-03-01 02:40:12 +0300

    Doom emacs: e on dashboard

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -34,6 +34,8 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one)
+(setq! fancy-splash-image
+       (expand-file-name "misc/emacs-e.svg" doom-private-dir))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
