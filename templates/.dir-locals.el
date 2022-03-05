09c5373d04f4b0dd818f97e991f830cc09e691aa
Constantine Bulany <robpirojk@gmail.com>
2022-03-05 05:03:46 +0300

    Moved templates

---
+++ templates/.dir-locals.el
@@ -0,0 +1,6 @@
+((nil . ((projectile-project-compilation-dir . "<build_dir>")
+         (projectile-project-configure-cmd . "<configure_cmd>")
+         (projectile-project-compilation-cmd . "<compilation_cmd>")
+         (projectile-project-run-cmd . "<run_cmd>")
+         (projectile-project-test-cmd . "<test_cmd>")
+         )))
